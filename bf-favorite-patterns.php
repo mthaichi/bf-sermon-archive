bc6a14eac2d6e9b907c4de9bf05d6fbd732f0960
Taichi Maruyama <maruyama@breadfish.jp>
2023-04-07 23:35:13 +0900

    fix: version

--- bf-favorite-patterns.php
+++ bf-favorite-patterns.php
@@ -4,7 +4,7 @@
  * Description:       BREADFISHで用意したパターンを読み込みます。
  * Requires at least: 6.1.1
  * Requires PHP:      7.4
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
